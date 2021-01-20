c0968f1affd87e4447c6731d7658322ddb15fb3c
NuraMostafa <64865902+NuraMostafa@users.noreply.github.com>
2021-01-20 02:12:20 +0200

    1

---
+++ SignUp.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>COVID</title>
+	<link rel="icon" href="img/favicon.png">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+
+	<link rel="stylesheet" href="css/themify-icons.css">
+
+	<link rel="stylesheet" href="css/flaticon.css">
+
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/nice-select.css">
+
+	<link rel="stylesheet" href="css/slick.css">
+
+	<link rel="stylesheet" href="css/style.css">
+	<style>
+	body {
+		padding: 20px;
+	}
+	h1 {
+		
+		font-weight: 700;
+		color: #ffffff;
+		font-size: 44px;
+		
+	}
+	
+</style>
+</head>
+<body>
+
+	<section class="regervation_part section_padding">
+		<div class="container">
+			<div class="row align-items-center regervation_content">
+				<div class="col-lg-7">
+					<div class="regervation_part_iner">
+						<form>
+							<h1>Register</h1>
+							<br/><br/>
+							<div class="form-row">
+								<div class="form-group col-md-6">
+
+									<input  class="form-control" id="inputEmail4" placeholder="FirstName">
+									<input  class="form-control" id="inputEmail4" placeholder="LastName">
+									<input  type="email" class="form-control" id="inputPassword4" placeholder="Email address">
+									<input  class="form-control" id="inputEmail4" placeholder="Username">
+									<input  type="password" class="form-control" id="inputEmail4" placeholder="Password">
+									<input type="password" class="form-control" id="inputEmail4" placeholder="Confirm Password">
+
+									<select class="form-control" id="Select">
+										<option value="1" selected>Select Gender</option>
+										<option value="2">Male</option>
+										<option value="3">Female</option>
+
+										<input type="date" class="form-control" id="birthday" placeholder="Your Birthday">
+
+									</select>
+
+
+
+
+								</div>
+								<br/><br/>
+
+
+
+							</div>
+							<div class="regerv_btn">
+								<a href="#" class="btn_2">Submit</a>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="col-lg-5 col-md-6">
+					<div class="reservation_img">
+						<img src="img/reservation.png" alt="" class="reservation_img_iner">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
