096c52d7d0e7e02827312f802a66b67e8a87a448
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-03-25 22:59:58 +0200

    Update register.php

--- register.php
+++ register.php
@@ -130,13 +130,79 @@ h1 {
 }
 </style> -->
 <body>
-<?php include("nav.php"); ?>
-<section class="regervation_part section_padding">
+<?php include("header.php"); ?>
+
+
+
+
+
+<!-- <div class="row justify-content-between align-items-center">
+
+<div class="col-md-6 col-lg-6">
+	<h1> </h1>
+<div class="about_us_img">
+<img src="img/top_service.png" alt="">
+</div>
+</div>
+
+<div class="col-md-6 col-lg-5">
+	
+			<div class="row align-items-center regervation_content">
+				<div class="col-lg-7">
+					<div class="regervation_part_iner">
+						<form>
+							
+							<br/><br/>
+							<div class="form-row">
+								   <form class="form-group col-md-6">
+								   	<label>CT scan</label>
+     <input  class="form-control" id="inputEmail4" >
+     <input type="file" id="myFile" name="filename">
+      <label>CRP test</label>
+     <input  class="form-control" id="inputEmail4" >
+           <label>D-Dimer test</label>
+     <input  class="form-control" id="inputEmail4" >
+           <label>Ferritin</label>
+     <input  class="form-control" id="inputEmail4" >
+           <label>LDH</label>
+     <input  class="form-control" id="inputEmail4" >
+           <label>ALT</label>
+     <input  class="form-control" id="inputEmail4" >
+           <label>AST</label>
+     <input  class="form-control" id="inputEmail4" >
+          <label>CBC</label>
+     <input  class="form-control" id="inputEmail4" >
+     <b></b>
+  
+    
+								<a href="#" class="btn_2">Submit</a>
+							
+
+        </form>
+								
+
+									
+								
+
+
+
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section> -->
+
+
+<section class="about_us padding_top">
 <div class="container">
     <br><br>
-    <h1 style="color:white;">Register</h1><br>
+    <h1 >Register</h1><br>
     
-    <div class="row align-items-center regervation_content">
+    <div class="row justify-content-between align-items-center">
 				<div class="col-lg-7">
 					<div class="regervation_part_iner">
 
@@ -144,15 +210,15 @@ h1 {
 								<div class="form-group col-md-6">
 
     <form method="POST" action="" enctype="multipart/form-data" >
-    <h4 style="color:white;">Username</h4>
+    <h4 >Username</h4>
         <input  class="form-control" name = "username" id="inputEmail4" placeholder="Username"><br>
-        <h4 style="color:white;">Email</h4>
+        <h4 >Email</h4>
         <input type="email" class="form-control" placeholder="Enter your email" required name="email" onkeyup="filter(this)" id="registeremail"><br>
-        <h4 style="color:white;">Password</h4>
+        <h4 >Password</h4>
         <input type="password" class="form-control" placeholder="Enter your password" required name="password" onkeyup="filter(this)" id="registerpassword"><br>
-        <h4 style="color:white;">Confirm Password</h4>
+        <h4 >Confirm Password</h4>
         <input type="password" class="form-control" id="inputEmail4" placeholder="Confirm Password" required name="Confirmpassword" onkeyup="filter(this)" id="registerconfirmpassword"><br>
-        <h4 style="color:white;">Select Gender</h4>
+        <h4 >Select Gender</h4>
        <select name="gender" class="form-control" id="Select">
 			<option selected>Select Gender</option>
 			<option >Male</option>
@@ -161,7 +227,7 @@ h1 {
 
 			</select required>
             <br>
-            <h4 style="color:white;">Date of Birth</h4>
+            <h4 >Date of Birth</h4>
             <input type="date" name= "dateofbirth" class="form-control" id="birthday" placeholder="Your Birthday">
 
       <br>
@@ -190,5 +256,6 @@ h1 {
 <script>
     new WOW().init();
 </script>
+<?php include("footer.php"); ?>
 </body>
 </html>
