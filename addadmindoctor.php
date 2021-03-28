ce99dec1ac194ce93c76718a1604291ab390caac
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-03-28 16:09:53 +0200

    Update and rename AddDoctor.php to addadmindoctor.php

---
+++ addadmindoctor.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>COVID</title>
+	<link rel="icon" href="img/favicon.png">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+
+	<link rel="stylesheet" href="css/themify-icons.css">
+
+	<link rel="stylesheet" href="css/flaticon.css">
+
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/nice-select.css">
+
+	<link rel="stylesheet" href="css/slick.css">
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<br/><br/>
+    <?php include("header.php"); ?>
+
+	
+	<section class="about_us padding_top">
+
+<div class="container">
+
+<div class="row justify-content-between align-items-center">
+
+<div class="col-md-6 col-lg-6">
+<div >
+<img src="img/service.png" >
+</div>
+</div>
+
+
+
+<div class="col-md-6 col-lg-5">
+	
+			<div class="row align-items-center regervation_content">
+				<div class="col-lg-7">
+					<div class="regervation_part_iner">
+						<form>
+                        <h1>Add User</h1> 
+							<br/><br/>
+							<div class="form-row">
+								   <form class="form-group col-md-6">
+								   
+                                   <h4>Email:</h4> 
+									<input  type="email" class="form-control" id="Email" placeholder="Email address"> <br/><br/>
+
+                                    <h4>Password:</h4>
+									<input  type="password" class="form-control" id="password" placeholder="Password"><br/><br/>
+
+                                    <h4>Confirm Password:</h4>
+									<input type="password" class="form-control" id="inputEmail4" placeholder="Confirm Password"><br/><br/>
+
+                                    <h4>Select Type:</h4>
+                                    <select class="form-control" id="SelectType">
+										<option value="1" selected>Select</option>
+										<option value="2">Male</option>
+										<option value="3">Female</option>					
+									</select><br/><br/>
+
+                                    <h4>Select Gender:</h4>
+									<select class="form-control" id="SelectGender">
+										<option value="1" selected>Select</option>
+										<option value="2">Male</option>
+										<option value="3">Female</option>					
+									</select><br/><br/>
+
+                                    <h4>Username:</h4>
+									<input  class="form-control" id="username" placeholder="Username"><br/><br/>
+                                    
+                                    <h4>Date of Birth:</h4>
+                                    <input type="date" class="form-control" id="birthday" placeholder="Your Birthday"><br/><br/>
+    
+								<a href="adminview.php" class="btn_2">Submit</a><br/><br/>
+							
+
+        </form>
+
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<?php include("footer.php"); ?>
+</body>
+
+</html>
+
+						
