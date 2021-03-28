4f60a1fe1dfad8569db6ab8605c7ed0f40e277eb
nourhanateffarag <64866274+nourhanateffarag@users.noreply.github.com>
2021-03-29 00:59:15 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,26 @@ if(isset($_SESSION['userID'])){
     header("Location:index.php");
 }
 
-if(isset($_POST['submit'])){
+if(isset($_POST['submit1'])){
+  $image = $_FILES['image']['name'];
+  $target = "images/" . basename($image);
+  $query = "INSERT INTO tests (`CPR`, `Ferritin`, `LDH`, `ALT`, `CBC`, `DDimer`, `AST`, `email`, `image`) VALUES ('Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', '$target')";
+  $result = mysqli_query($connection, $query);
+  if($result){
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+      echo "<script>alert('Image uploaded successfully, thank you.')</script>";
+    }
+    else {
+      echo "<script>alert('Error uploading image, please try again.')</script>";
+    }
+  }
+  else {
+    echo "<script>alert('Error performing query, please try again.')</script>";
+  }
+}
+
+
+if(isset($_POST['submit2'])){
     $CPR = $_POST['CPR'];
     $Ferritin = $_POST['Ferritin'];
     $LDH = $_POST['LDH'];
@@ -14,34 +33,12 @@ if(isset($_POST['submit'])){
     $DDimer = $_POST['DDimer'];
     $AST = $_POST['AST'];
     $email = $_POST['email'];
-    $image = $_FILES['image']['name'];
-    $target = "images/" . basename($image);
-
-    $query = "INSERT INTO tests (`CPR`, `Ferritin`, `LDH`, `ALT`, `CBC`, `DDimer`, `AST`, `email`, `image`) VALUES ('$CPR', '$Ferritin', '$LDH', '$ALT', '$CBC', '$DDimer', '$AST', '$email', '$target')";
+    
+    $query = "INSERT INTO tests (`CPR`, `Ferritin`, `LDH`, `ALT`, `CBC`, `DDimer`, `AST`, `email`, `image`) VALUES ('$CPR', '$Ferritin', '$LDH', '$ALT', '$CBC', '$DDimer', '$AST', '$email', 'Empty')";
     $result = mysqli_query($connection, $query);
 
     if($result){
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-            echo "<script>alert('data success.')</script>";
-        } 
-        else {
-            echo "<script>alert('data fail.')</script>";
-        }
-        $query2 = "SELECT * FROM tests WHERE email = '$email'";
-        $result2 = mysqli_query($connection, $query2);
-        if($result2){
-            if(mysqli_num_rows($result2) > 0 ){
-                $row = mysqli_fetch_array($result2);
-                $userID = $row[0];
-                $userEmail = $row[8];
-                $_SESSION['userID'] = $userID;
-                $_SESSION['userEmail'] = $userEmail;
-                echo "<script>location.replace('index.php');</script>";
-            }
-        }
-        else {
-            echo "<script>alert('Error performing query, please try agian.')</script>";
-        }
+      echo "<script>alert('Data submitted successfully, thank you.')</script>";
     }
     else {
         echo "<script>alert('Error performing query, please try agian.')</script>";
@@ -56,7 +53,7 @@ if(isset($_POST['submit'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>tests</title>
+    <title>Tests</title>
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
@@ -104,19 +101,28 @@ if(isset($_POST['submit'])){
 <?php include("nav.php"); ?>
 <div class="container">
     <br><br>
-    <h1 style="text-align: center;">Register</h1><br>
-  <div class="form">
+    <h1 style="text-align: center;" id="title">Do you wish to submit an image or the full data?</h1><br>
+    <div id="buttonsdiv">
+    <button onclick="form1();" class="btn btn-dark">Image Only</button>
+    <button onclick="form2()" class="btn btn-info">Full Data</button>
+  </div>
+    <div class="form" id="form1">
     <form method="POST" action="" enctype="multipart/form-data" style="text-align: center;">
-        <input type="email" class="x" placeholder="Enter your email..." required name="email" onkeyup="filter(this)" id="email"><br><br>
-        <input type="number" class="x" placeholder="CPR" required name="CPR" onkeyup="filter(this)" id="CPR"><br><br>
-        <input type="number" class="x" placeholder="Ferritin" required name="Ferritin" onkeyup="filter(this)" id="Ferritin"><br><br>
-        <input type="number" class="x" placeholder="LDH" required name="LDH" onkeyup="filter(this)" id="LDH"><br><br>
-        <input type="number" class="x" placeholder="ALT" required name="ALT" onkeyup="filter(this)" id="ALT"><br><br>
-        <input type="number" class="x" placeholder="CBC" required name="CBC" onkeyup="filter(this)" id="CBC"><br><br>
-        <input type="number" class="x" placeholder="DDimer" required name="DDimer" onkeyup="filter(this)" id="DDimer"><br><br>
-        <input type="number" class="x" placeholder="AST" required name="AST" onkeyup="filter(this)" id="AST"><br><br>
-        <input type="file" accept="image/*" name="image" required><br><br>
-        <button type="submit" name="submit" class="sub" class="btn btn-primary">Submit</button>
+        <input type="file" accept="image/*" name="image"><br><br>
+        <button type="submit" name="submit1" class="sub" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+  <div class="form" id="form2">
+    <form method="POST" action="" enctype="multipart/form-data" style="text-align: center;">
+        <input type="email" class="x" placeholder="Enter your email..." name="email" onkeyup="filter(this)" id="email"><br><br>
+        <input type="number" class="x" placeholder="CPR" name="CPR" onkeyup="filter(this)" id="CPR"><br><br>
+        <input type="number" class="x" placeholder="Ferritin" name="Ferritin" onkeyup="filter(this)" id="Ferritin"><br><br>
+        <input type="number" class="x" placeholder="LDH" name="LDH" onkeyup="filter(this)" id="LDH"><br><br>
+        <input type="number" class="x" placeholder="ALT" name="ALT" onkeyup="filter(this)" id="ALT"><br><br>
+        <input type="number" class="x" placeholder="CBC" name="CBC" onkeyup="filter(this)" id="CBC"><br><br>
+        <input type="number" class="x" placeholder="DDimer" name="DDimer" onkeyup="filter(this)" id="DDimer"><br><br>
+        <input type="number" class="x" placeholder="AST" name="AST" onkeyup="filter(this)" id="AST"><br><br>
+        <button type="submit" name="submit2" class="sub" class="btn btn-primary">Submit</button>
     </form>
 </div>
 </div>
@@ -127,6 +133,21 @@ if(isset($_POST['submit'])){
 <script src="js/main.js"></script>
 <script>
     new WOW().init();
+</script>
+<script type="text/javascript">
+  $("#form1").hide();
+  $("#form2").hide();
+  function form1(){
+    $("#form1").show();
+    $("#title").html("Please submit the required data.");
+    $("#buttonsdiv").hide();
+  }
+
+  function form2(){
+    $("#form2").show();
+    $("#title").html("Please submit the required data.");
+    $("#buttonsdiv").hide();
+  }
 </script>
 </body>
 </html>
