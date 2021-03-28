d9e967da65f3e923efadad28b61374a531ce9cdf
nourhanateffarag <64866274+nourhanateffarag@users.noreply.github.com>
2021-03-28 17:39:06 +0200

    Add files via upload

--- covid.sql
+++ covid.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2021 at 03:40 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- Generation Time: Mar 28, 2021 at 05:37 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,6 +40,13 @@ CREATE TABLE `tests` (
   `image` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tests`
+--
+
+INSERT INTO `tests` (`id`, `CPR`, `Ferritin`, `LDH`, `ALT`, `CBC`, `DDimer`, `AST`, `email`, `image`) VALUES
+(4, '10', '20', '30', '40', '50', '16', '20', 'nouraatef43@yahoo.com', 'images/pirate.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -52,24 +59,28 @@ CREATE TABLE `users` (
   `Password` varchar(300) NOT NULL,
   `User_type` varchar(300) NOT NULL,
   `Gender` varchar(300) NOT NULL,
-  `confirmpassword` varchar(50) NOT NULL,
-  `Username` varchar(100) NOT NULL
+  `Username` varchar(100) NOT NULL,
+  `dateofbirth` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `Password`, `User_type`, `Gender`, `confirmpassword`, `Username`) VALUES
+INSERT INTO `users` (`id`, `email`, `Password`, `User_type`, `Gender`, `Username`, `dateofbirth`) VALUES
 (11, 'mariam@gmail.com', '123456', 'Patient', '', '', ''),
 (12, 'nourhan@gmail.com', '123456', 'Patient', '', '', ''),
 (15, 'nada@gmail.com', '123456', ' ', '', '', ''),
-(16, 'doaa@gmail.com', '123456', '', 'Doctor', '123456', ''),
-(17, 'marwa@gmail.com', '789', '', 'Select', '789', ''),
-(18, 'mariam@gmail.com', '123456', '', 'female', '123456', ''),
-(19, 'salma@gmail.com', '123456', 'Patient', 'female', '123456', ''),
-(20, 'nouraatef43@yahoo.com', '123456', 'Patient', 'female', '123456', ''),
-(21, 'amr@gmail.com', '123456', 'Doctor', 'female', '123456', 'amrmohamed');
+(16, 'doaa@gmail.com', '123456', '', 'Doctor', '', ''),
+(17, 'marwa@gmail.com', '789', '', 'Select', '', ''),
+(18, 'mariam@gmail.com', '123456', '', 'female', '', ''),
+(19, 'salma@gmail.com', '123456', 'Patient', 'female', '', ''),
+(20, 'nouraatef43@yahoo.com', '123456', 'Patient', 'female', '', ''),
+(21, 'amr@gmail.com', '123456', 'Doctor', 'female', 'amrmohamed', ''),
+(22, 'nouraatef43@yahoo.com', '123456', 'Patient', '', '', ''),
+(23, 'nourhana307@gmail.com', '123456', 'Patient', '', '', ''),
+(24, 'enas@gmail.com', '123456', '2', '', '', '2021-03-12'),
+(25, 'dodo@gmail.com', '123456', '2', 'Male', '', '2021-03-25');
 
 --
 -- Indexes for dumped tables
@@ -95,13 +106,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tests`
 --
 ALTER TABLE `tests`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
