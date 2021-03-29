5307500e930491cf6ecdc68d58cc92f46a0e9b7e
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-03-29 04:59:45 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,30 +1,9 @@
 <?php
 require_once("connect.php");
 
-if(isset($_SESSION['userID'])){
-    header("Location:index.php");
-}
-
-if(isset($_POST['submit1'])){
-  $image = $_FILES['image']['name'];
-  $target = "images/" . basename($image);
-  $query = "INSERT INTO tests (`CPR`, `Ferritin`, `LDH`, `ALT`, `CBC`, `DDimer`, `AST`, `email`, `image`) VALUES ('Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', '$target')";
-  $result = mysqli_query($connection, $query);
-  if($result){
-    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-      echo "<script>alert('Image uploaded successfully, thank you.')</script>";
-    }
-    else {
-      echo "<script>alert('Error uploading image, please try again.')</script>";
-    }
-  }
-  else {
-    echo "<script>alert('Error performing query, please try again.')</script>";
-  }
-}
 
 
-if(isset($_POST['submit2'])){
+if(isset($_POST['submit'])){
     $CPR = $_POST['CPR'];
     $Ferritin = $_POST['Ferritin'];
     $LDH = $_POST['LDH'];
@@ -33,17 +12,22 @@ if(isset($_POST['submit2'])){
     $DDimer = $_POST['DDimer'];
     $AST = $_POST['AST'];
     $email = $_POST['email'];
-    
-    $query = "INSERT INTO tests (`CPR`, `Ferritin`, `LDH`, `ALT`, `CBC`, `DDimer`, `AST`, `email`, `image`) VALUES ('$CPR', '$Ferritin', '$LDH', '$ALT', '$CBC', '$DDimer', '$AST', '$email', 'Empty')";
-    $result = mysqli_query($connection, $query);
-
-    if($result){
-      echo "<script>alert('Data submitted successfully, thank you.')</script>";
+  $image = $_FILES['image']['name'];
+  $target = "img/" . basename($image);
+  $query = "INSERT INTO tests (`CPR`, `Ferritin`, `LDH`, `ALT`, `CBC`, `DDimer`, `AST`, `email`, `image`) VALUES ('$CPR', '$Ferritin', '$LDH', '$ALT', '$CBC', '$DDimer', '$AST', '$email','$target')";
+  $result = mysqli_query($connection, $query);
+  if($result){
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+      echo "<script>alert('Image uploaded successfully, thank you.')</script>";
     }
-    else {
-        echo "<script>alert('Error performing query, please try agian.')</script>";
-    }
+    echo "<script>alert('Data Uploaded successfully.')</script>";
+  }
+  else {
+    echo "<script>alert('Error performing query, please try again.')</script>";
+  }
 }
+
+
 ?>
 
 <!doctype html>
@@ -55,77 +39,61 @@ if(isset($_POST['submit2'])){
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tests</title>
     <link rel="stylesheet" href="css/animate.css">
+    <link rel="icon" href="img/favicon.png">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
 </head>
-<style>
-  body {
-            text-align: center;
-            align: center;
 
-        }
-
-.x {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-
-.sub {
-  width: 300px;
-  background-color: blue;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.sub:hover {
-  background-color: #45a049;
-}
-
-.form{
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-</style>
 <body>
-<?php include("nav.php"); ?>
+<?php include("header.php"); ?>
+<section class="about_us padding_top">
 <div class="container">
-    <br><br>
-    <h1 style="text-align: center;" id="title">Do you wish to submit an image or the full data?</h1><br>
-    <div id="buttonsdiv">
-    <button onclick="form1();" class="btn btn-dark">Image Only</button>
-    <button onclick="form2()" class="btn btn-info">Full Data</button>
-  </div>
-    <div class="form" id="form1">
-    <form method="POST" action="" enctype="multipart/form-data" style="text-align: center;">
-        <input type="file" accept="image/*" name="image"><br><br>
-        <button type="submit" name="submit1" class="sub" class="btn btn-primary">Submit</button>
-    </form>
+<div class="row justify-content-between align-items-center">
+
+<div class="col-md-6 col-lg-6">
+<div >
+<img src="img/top_service.png" alt="">
 </div>
-  <div class="form" id="form2">
-    <form method="POST" action="" enctype="multipart/form-data" style="text-align: center;">
-        <input type="email" class="x" placeholder="Enter your email..." name="email" onkeyup="filter(this)" id="email"><br><br>
+</div>
+    
+    <div class="col-md-6 col-lg-5">
+	
+    <div class="row align-items-center regervation_content">
+    
+        <div class="col-lg-7">
+        <h1>Tests</h1> 
+        <br><br>
+            <div class="regervation_part_iner">
+    <form method="POST" action="" enctype="multipart/form-data" >
+       
+       
+        <input style="color: black;" type="file" accept="img/*" name="image"><br><br>
+        <h4 >Email:</h4>
+        <input type="email" class="x" placeholder="Enter your email..." required name="email" onkeyup="filter(this)" id="email"><br><br>
+        <h4 >CRP:</h4>
         <input type="number" class="x" placeholder="CPR" name="CPR" onkeyup="filter(this)" id="CPR"><br><br>
+        <h4 >Ferritin:</h4>
         <input type="number" class="x" placeholder="Ferritin" name="Ferritin" onkeyup="filter(this)" id="Ferritin"><br><br>
+        <h4 >LDH:</h4>
         <input type="number" class="x" placeholder="LDH" name="LDH" onkeyup="filter(this)" id="LDH"><br><br>
+        <h4 >ALT:</h4>
         <input type="number" class="x" placeholder="ALT" name="ALT" onkeyup="filter(this)" id="ALT"><br><br>
+        <h4 >CBC:</h4>
         <input type="number" class="x" placeholder="CBC" name="CBC" onkeyup="filter(this)" id="CBC"><br><br>
+        <h4 >D-Dimer:</h4>
         <input type="number" class="x" placeholder="DDimer" name="DDimer" onkeyup="filter(this)" id="DDimer"><br><br>
+        <h4 >AST:</h4>
         <input type="number" class="x" placeholder="AST" name="AST" onkeyup="filter(this)" id="AST"><br><br>
-        <button type="submit" name="submit2" class="sub" class="btn btn-primary">Submit</button>
+
+        <div class="regerv_btn"><button type="submit" name="submit"class="btn_2">Submit</button></div><br><br>
+
     </form>
+    </div>
+    </div>
 </div>
 </div>
+
 <script src="js/jquery-3.4.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/wow.min.js"></script>
@@ -134,20 +102,9 @@ if(isset($_POST['submit2'])){
 <script>
     new WOW().init();
 </script>
-<script type="text/javascript">
-  $("#form1").hide();
-  $("#form2").hide();
-  function form1(){
-    $("#form1").show();
-    $("#title").html("Please submit the required data.");
-    $("#buttonsdiv").hide();
-  }
 
-  function form2(){
-    $("#form2").show();
-    $("#title").html("Please submit the required data.");
-    $("#buttonsdiv").hide();
-  }
 </script>
+</section>
+<?php include("footer.php"); ?>
 </body>
 </html>
