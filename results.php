ee8a7876f772bdd40979e4fdcb75b2487d89ed97
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-03-29 13:44:16 +0200

    Add files via upload

---
+++ results.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>COVID</title>
+	<link rel="icon" href="img/favicon.png">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+
+	<link rel="stylesheet" href="css/themify-icons.css">
+
+	<link rel="stylesheet" href="css/flaticon.css">
+
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/nice-select.css">
+
+	<link rel="stylesheet" href="css/slick.css">
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<br/><br/>
+	<?php include("header.php"); ?>
+	<section class="about_us padding_top">
+	
+			
+	
+<div class="container">
+<div class="row justify-content-between align-items-center">
+<div class="col-md-6 col-lg-5">
+<div class="row align-items-center regervation_content">
+<div class="col-lg-7">
+<div class="regervation_part_iner">
+<form><br/><br/>
+<h1>Results</h1>
+<br/><br/>
+<div class="form-row">
+							
+<form class="form-group col-md-6">
+
+<h4>ID:</h4> <br><br>
+
+
+<h4>Name:</h4> <br><br>
+
+
+<h4>Gender:</h4> <br><br>
+
+
+<h4>Status:</h4>
+
+<br><br>
+              <div class="regerv_btn">
+
+        <button type="submit" name="print" class="btn_2" onclick="window.print()"> Print Result</button>
+
+        </div>
+        <br><br>
+ 
+        <div class="regerv_btn">
+        <a href="testnourhan.php" class="btn_2">Add New Test</a>
+        </div>
+
+							
+
+        </form>
+
+		<br/><br/>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<?php include("footer.php"); ?>
+</body>
+</html>
+
+						
