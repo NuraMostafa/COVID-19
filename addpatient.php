548ad9e7391ad447b9d5fd1067fa21ba7bd56ac8
nourhanateffarag <64866274+nourhanateffarag@users.noreply.github.com>
2021-03-29 23:32:37 +0200

    Update addpatient.php

--- addpatient.php
+++ addpatient.php
@@ -1,49 +1,4 @@
-<?php
-require_once("connect.php");
 
-if(isset($_SESSION['userID'])){
-    header("Location:addpatientnourhan.php");
-}
-
-if(isset($_POST['submit'])){
-    $email = $_POST['email'];
-    $Username = $_POST['Username'];
-    $Gender = $_POST['Gender'];
-    $dateofbirth = $_POST['dateofbirth'];
-
-    $query = "INSERT INTO patients(Username,email,Gender,dateofbirth) VALUES ('$Username','$email','$Gender','$dateofbirth')";
-
-    $result = mysqli_query($connection, $query);
-
-    if($result){
-        $query2 = "SELECT * FROM patients WHERE email = '$email'";
-        $result2 = mysqli_query($connection, $query2);
-        if($result2){
-            if(mysqli_num_rows($result2) > 0 ){
-                $row = mysqli_fetch_array($result2);
-                $userID = $row[0];
-                $Username = $row[1];
-                $userEmail = $row[2];
-              
-
-                $_SESSION['userID'] = $userID;
-                $_SESSION['Username'] = $Username;
-                $_SESSION['userEmail'] = $userEmail;
-
-
-                echo "<script>location.replace('doctorview.php');</script>";
-            }
-        }
-        else {
-            echo "<script>alert('Error performing query, please try agian.')</script>";
-        }
-        
-    }
-    else {
-        echo "<script>alert('Error performing query, please try agian.')</script>";
-    }
-}
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -86,7 +41,7 @@ if(isset($_POST['submit'])){
                     <div class="form-row">
                 <div class="form-group col-md-6">
 
-    <form method="POST" action="" enctype="multipart/form-data" >
+    <form method="POST" action="addpatientphp.php" enctype="multipart/form-data" >
     <h4 >Username</h4>
         <input  class="x" name = "Username" id="inputEmail4" placeholder="Username"><br>
         <h4 >Email</h4>
