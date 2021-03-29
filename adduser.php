a5c815d62fa75f5d6e52b735e6a09a39cdf9426c
Sara379 <61588221+Sara379@users.noreply.github.com>
2021-03-29 19:05:00 +0200

    Update adduser.php

--- adduser.php
+++ adduser.php
@@ -109,15 +109,15 @@ if(isset($_POST['submit'])){
 
     <form method="POST" action="" enctype="multipart/form-data" >
     <h4 >Username</h4>
-        <input  class="form-control" name = "Username" id="inputEmail4" placeholder="Username"><br>
+        <input  class="x" name = "Username" id="inputEmail4" placeholder="Username"><br>
         <h4 >Email</h4>
-        <input type="email" class="form-control" placeholder="Enter your email" required name="email" onkeyup="filter(this)" id="registeremail"><br>
+        <input type="email" class="x" placeholder="Enter your email" required name="email" onkeyup="filter(this)" id="registeremail"><br>
         <h4 >Password</h4>
-        <input type="password" class="form-control" placeholder="Enter your password" required name="password" onkeyup="filter(this)" id="registerpassword"><br>
+        <input type="password" class="x" placeholder="Enter your password" required name="password" onkeyup="filter(this)" id="registerpassword"><br>
         <h4 >Confirm Password</h4>
-        <input type="password" class="form-control" id="inputEmail4" placeholder="Confirm Password" required name="Confirmpassword" onkeyup="filter(this)" id="registerconfirmpassword"><br>
+        <input type="password" class="x" id="inputEmail4" placeholder="Confirm Password" required name="Confirmpassword" onkeyup="filter(this)" id="registerconfirmpassword"><br>
         <h4 >Select Usertype</h4>
-       <select name="User_type" class="form-control" id="Select">
+       <select name="User_type" class="x" id="Select">
       <option selected>Select Usertype</option>
       <option >Admin</option>
       <option >Doctor</option>
@@ -127,7 +127,7 @@ if(isset($_POST['submit'])){
 
 
         <h4 >Select Gender</h4>
-       <select name="Gender" class="form-control" id="Select">
+       <select name="Gender" class="x" id="Select">
       <option selected>Select Gender</option>
       <option >Male</option>
       <option >Female</option>
@@ -135,7 +135,7 @@ if(isset($_POST['submit'])){
       </select required>
             <br>
             <h4 >Date of Birth</h4>
-            <input type="date" name= "dateofbirth" class="form-control" id="birthday" placeholder="Your Birthday">
+            <input type="date" name= "dateofbirth" class="x" id="birthday" placeholder="Your Birthday">
 
       <br>
         <!-- <input type="file" accept="image/*" name="image" ><br><br> -->
