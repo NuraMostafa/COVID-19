359dfe893f722a72bdb92227eca9f4e2743355e3
nourhanateffarag <64866274+nourhanateffarag@users.noreply.github.com>
2021-06-07 22:42:54 +0200

    Update ViewUser.php

--- app/view/ViewUser.php
+++ app/view/ViewUser.php
@@ -20,14 +20,10 @@ class ViewUser extends View{
 		
 	function loginForm(){
 		$str='<form method="POST" action="user.php" enctype="multipart/form-data" >
-			         <div class= "input-container">
-			    <i class="fa fa-envelope icon"></i>
-			        <input type="email" class="input"  placeholder="Enter your email" required name="email" onkeyup="filter(this)" id="registeremail"><br>
-			        </div>
-			         <div class= "input-container">
-		         <i class="fa fa-key icon"></i>
-			        <input type="password" class="input" placeholder="Enter your password" required name="password" onkeyup="filter(this)" id="registerpassword"><br>
-			        </div>
+			        <h4 >Email</h4>
+			        <input type="email" class="x" placeholder="Enter your email" required name="email" onkeyup="filter(this)" id="registeremail"><br>
+			        <h4 >Password</h4>
+			        <input type="password" class="x" placeholder="Enter your password" required name="password" onkeyup="filter(this)" id="registerpassword"><br>
 			        <div class="regerv_btn">
 			        <button type="submit" name="login"class="btn_2">Login</button>
 			        </div>
@@ -38,41 +34,31 @@ class ViewUser extends View{
 
 	function signupForm(){
 		$str='<form method="POST" action="user.php?action=insert" enctype="multipart/form-data" >
-		       <div class= "input-container">
-			    <i class="fa fa-user icon"></i>
-		        <input  class="input" name="username" id="inputEmail4" placeholder="Username"><br>
-		        </div>
-		        <div class= "input-container">
-		         <i class="fa fa-envelope icon"></i>
-		        <input type="email" class="input" placeholder="Enter your email" required name="email" onkeyup="filter(this)" id="registeremail"><br>
-
-		      </div>
-		      <div class= "input-container">
-		         <i class="fa fa-key icon"></i>
-		        <input type="password" class="input" placeholder="Enter your password" required name="password" onkeyup="filter(this)" registerpassword"><br>
-		        </div>
-		         <div class= "input-container">
-		         <i class="fa fa-key icon"></i>
-		        <input type="password" class="input" id="inputEmail4" placeholder="Confirm Password" required name="Confirmpassword" onkeyup="filter(this)" id="registerconfirmpassword"><br>
-		       </div>
-		       <div class= "input-container">
-		         <i class="fa fa-transgender icon"></i>
-		        <select name="gender" class="input" id="Select">
+			    <h4 >Username</h4>
+		        <input  class="x" name="username" id="inputEmail4" placeholder="Username"><br>
+		        <h4 >Email</h4>
+		        <input type="email" class="x" placeholder="Enter your email" required name="email" onkeyup="filter(this)" id="registeremail"><br>
+		        <h4 >Password</h4>
+		        <input type="password" class="x" placeholder="Enter your password" required name="password" onkeyup="filter(this)" registerpassword"><br>
+		        <h4 >Confirm Password</h4>
+		        <input type="password" class="x" id="inputEmail4" placeholder="Confirm Password" required name="Confirmpassword" onkeyup="filter(this)" id="registerconfirmpassword"><br>
+		        <h4 >Select Gender</h4>
+		        <select name="gender" class="x" id="Select">
 			        <option selected>Select Gender</option>
 			        <option >Male</option>
 			        <option >Female</option>
 		        </select required>
-			    </div>
-			     <div class= "input-container">
-		         <i class="fa fa-calendar icon"></i>
-			    <input type="date" name= "dateofbirth" class="input" id="birthday" placeholder="Your Birthday">
-				</div>
+			    <br>
+			    <h4 >Date of Birth</h4>
+			    <input type="date" name= "dateofbirth" class="x" id="birthday" placeholder="Your Birthday">
+				<br>
 		        <div class="regerv_btn">
 		        	<button type="submit" name="submit"class="btn_2">SignUp</button>
 		        </div>
 			</form>';
 		return $str;
 	}
+
 	public function editForm(){
 		$str='<form action="profile.php?action=editaction" method="post">
 		<div>Email:</div><div> <input type="text" name="email" value="'.$this->model->getEmail().'"/></div><br>
@@ -211,22 +197,18 @@ class ViewUser extends View{
                     <th>ID</th>
                     <th>Username</th> 
                     <th>Gender</th> 
-                    <th>Email</th> 
                     <th>Edit</th> 
                     <th>Delete</th> 
-                    <th>AddTest</th> 
                   </tr>';
                   
         	if (count($PatientsArray) > 0) {
-		        for($i = 0 ; $i < count($PatientsArray); $i ++) {
+		        for($i = 0 ; $i < count($PatientsArray) ; $i ++) {
 			    	$str.='<tr>
-				            <td>'.$PatientsArray[$i]->getiD().'</td>
-				            <td>'.$PatientsArray[$i]->getuserName().'</td>
-				            <td>'.$PatientsArray[$i]->getuserName().'</td>
-				            <td>'.$PatientsArray[$i]->getgender().'</td>
+				            <td>'.$PatientsArray[$i]->getID().'</td>
+				            <td>'.$PatientsArray[$i]->getUserName().'</td>
+				            <td>'.$PatientsArray[$i]->getGender().'</td>
 				            <td><a href="editPatient.php?patientID='.$PatientsArray[$i]->getID().'"> Edit </a></td>
 				            <td><a href="doctor.php?action=deletePatient&patientID='.$PatientsArray[$i]->getID().'"> Delete </a></td>
-				            <td><a href="tests.php"> Add Test </a></td>
 
 			            </tr>';                    
 	            }
@@ -234,12 +216,19 @@ class ViewUser extends View{
               $str.= "<p style='color:black;'>No data to view</p>";
             }
             $str.= '</table>
+
+
 					<br> <br>
 					<div class="regerv_btn">
-					<a href="adduser.php" class="btn_2">Add Patient</a>
+					<a href="addPatient.php" class="btn_2">Add Patient</a>
 					</div>
-						</div>';
-        return $str;
+
+
+						</div>';  
+
+					return $str;
+
+     
 	}
 
 
@@ -272,16 +261,18 @@ class ViewUser extends View{
 						<td>Date Of Birth</td>
 						<td>'.$userData->getDateOfBirth().'</td>
 					</tr>
-					<tr>
-				            <td><a href="editPatientaccount.php?userID='.$userData->getID().'"> Edit </a></td>
+						<tr>
+				            <td><a href="edituser.php?userID='.$userData->getID().'"> Edit </a></td>
 				            <td><a href="profile.php?action=deleteUser&userID='.$userData->getID().'"> Delete </a></td>
-			            </tr>  
+			            </tr> 
 	            </table>
-			<br> <br>
-			<div class="regerv_btn">
-					<a href="tests.php" class="btn_2">Add Test</a>
+					<br> <br>
+					<div class="regerv_btn">
+					<a href="tests.php" class="btn_2">Add tests</a>
 					</div>
-		</div>';
+					</div>
+						</div>';
+		
         return $str;	
 	}
 
@@ -298,8 +289,7 @@ class ViewUser extends View{
 
 
 	public function viewEditUPatientData($patientData){
-		$str = '
-			<div class="form-group col-md-6">
+$str = '<div class="form-group col-md-6">
 			<form method="POST" action="" enctype="multipart/form-data" >
 			<h4 >Username</h4>
 				<input  class="x" name = "username" id="inputEmail4" placeholder="Username" value="'.$patientData->getUserName().'"><br>
@@ -327,60 +317,52 @@ class ViewUser extends View{
 				</div>
 				</div>
 			</form>
-		</div>
-		';
-		return $str;;
+		</div>';
+		return $str;
+	
 	}
 
 
 	
 	function testsform(){
-		$str=' <form method="POST" action="tests.php?action=addTest" enctype="multipart/form-data" style="   max-width: 100%;
-  margin: 0 auto;  
-  display: block;">
+		$str=' <form method="POST" action="tests.php?action=addTest" enctype="multipart/form-data" >
         
-        <div class= "input-container">
-		        
-        <input type="email" class="input" style="  -webkit-border-radius: 10px;
-                                                   -moz-border-radius: 10px;
-                                                    border-radius: 20px; 
-                                                    background-color: white;" 
-       placeholder="Enter your email..." required name="email" onkeyup="filter(this)" id="email"><br>
-        </div>
+        <h4 >Email:</h4>
+        <input type="email" class="x" placeholder="Enter your email..." required name="email" onkeyup="filter(this)" id="email"><br>
         <div class="input_wrapper">
-        <h4 style= " color: #114C56; font-size: 25px;">CRP:</h4>
-       <input type="number" class="input" placeholder="Enter CRP value" name="CPR" onkeyup="filter(this)" id="CPR" /><span style="margin-left:-40px; color: black;">mg/l</span>
-   
+        <h4 >CRP:</h4>
+    <input type="number" class="x" placeholder="CRP" name="CPR" onkeyup="filter(this)" id="CPR" />
+    <div class="unit">mg/l</div>
   </div>
   <div class="input_wrapper">
-  <h4 style= " color: #114C56; font-size: 25px;">Ferritin:</h4>
-    <input type="number" class="input" placeholder="Ferritin" name="Ferritin" onkeyup="filter(this)" id="Ferritin" /><span style="margin-left:-53px; color: black;">ng/mL</span>
-   
+  <h4 >Ferritin:</h4>
+    <input type="number" class="x" placeholder="Ferritin" name="Ferritin" onkeyup="filter(this)" id="Ferritin" />
+    <div class="unit">ng/mL</div>
   </div>
   <div class="input_wrapper">
-  <h4 style= " color: #114C56; font-size: 25px;">LDH:</h4>
-    <input type="number" class="input" placeholder="LDH" name="LDH" onkeyup="filter(this)" id="LDH" /><span style="margin-left:-37px; color: black;">U/l</span>
-    
+  <h4 >LDH:</h4>
+    <input type="number" class="x" placeholder="LDH" name="LDH" onkeyup="filter(this)" id="LDH" />
+    <div class="unit">U/l</div>
   </div>
   <div class="input_wrapper">
-  <h4 style= " color: #114C56; font-size: 25px;">ALT:</h4>
-    <input type="number" class="input" placeholder="ALT" name="ALT" onkeyup="filter(this)" id="ALT" /><span style="margin-left:-37px; color: black;">U/l</span>
-    
+  <h4 >ALT:</h4>
+    <input type="number" class="x" placeholder="ALT" name="ALT" onkeyup="filter(this)" id="ALT" />
+    <div class="unit">U/l</div>
   </div>
   <div class="input_wrapper">
-  <h4 style= " color: #114C56; font-size: 25px;">CBC:</h4>
-    <input  type="number" class="input" placeholder="CBC" name="CBC" onkeyup="filter(this)" id="CBC" /><span style="margin-left:-40px; color: black;">g/dl</span>
-   
+  <h4 >CBC:</h4>
+    <input  type="number" class="x" placeholder="CBC" name="CBC" onkeyup="filter(this)" id="CBC" />
+    <div class="unit">g/dl</div>
   </div>
   <div class="input_wrapper">
-  <h4 style= " color: #114C56; font-size: 25px;">D-Dimer:</h4>
-    <input type="number" class="input" placeholder="D-Dimer" name="DDimer" onkeyup="filter(this)" id="DDimer" /><span style="margin-left:-90px; color: black;">µg FEU/mL</span>
-   
+  <h4 >D-Dimer:</h4>
+    <input type="number" class="x" placeholder="D-Dimer" name="DDimer" onkeyup="filter(this)" id="DDimer" />
+    <div class="unit">µg FEU/mL</div>
   </div>
   <div class="input_wrapper">
-  <h4 style= " color: #114C56; font-size: 25px;">AST:</h4>
-    <input type="number" class="input" placeholder="AST" name="AST" onkeyup="filter(this)" id="AST" /><span style="margin-left:-37px; color: black;">U/l</span>
-    
+  <h4 >AST:</h4>
+    <input type="number" class="x" placeholder="AST" name="AST" onkeyup="filter(this)" id="AST" />
+    <div class="unit">U/l</div>
   </div>
         <div class="regerv_btn"><button type="submit" name="submit"class="btn_2">Add Test</button></div><br><br>
 
