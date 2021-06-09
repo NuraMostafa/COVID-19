31f71a6d1994b2fab838dccda3acf70000288b5f
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-06-09 03:44:04 +0200

    Update svm.py

--- python models/svm.py
+++ python models/svm.py
@@ -38,6 +38,25 @@ print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
 
 
-
-from google.colab import drive
-drive.mount('/content/drive/')
+#####################
+import pickle
+import cv2
+import numpy as np
+import keras
+import sys
+import os
+text_field = "/content/wahedbs.csv"
+file_name = "SVM.pkl"
+with open(file_name, "wb") as open_file:
+    pickle.dump(svclassifier, open_file)
+loaded_model = pickle.load(open(file_name, 'rb'))
+result1 = loaded_model.score(X_test, y_test)
+text=np.array(135, dtype=None)
+text[0]=  pd.read_csv(text_field)
+#how can we read textfield?????????????????????????????
+predict_result = result1.predict(text)
+if predict_result_max[0] == 0:
+    result = predict_result
+    for i in range(len(result[0])):
+        print(",")
+        print(result[0][i])
