930d64c24f09c7a799f54f1e9902da308e9f16dd
nourhanateffarag <64866274+nourhanateffarag@users.noreply.github.com>
2021-06-22 23:59:09 +0200

    Update results.php

--- public/results.php
+++ public/results.php
@@ -1,139 +1,109 @@
+<?php
+session_start();
+define('__ROOT__', "../app/");
+require_once(__ROOT__ . "model/Results.php");
+require_once(__ROOT__ . "controller/ResultsController.php");
+require_once(__ROOT__ . "view/ViewResult.php");
+
+$model = new Results();
+$controller = new ResultsController($model);
+$view = new ViewResult($controller, $model);
+
+$resultPageData = '';
+
+if(!isset($_SESSION['userType']) || $_SESSION['userType'] != 'Patient'){
+  header('Location: index.php');
+}
+
+
+?>
+
+
+
 <!doctype html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Result</title>
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="icon" href="assets/img/favicon.png">
 
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>COVID</title>
-	<link rel="icon" href="img/favicon.png">
+  <link rel="stylesheet" href="assets/css/themify-icons.css">
 
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/flaticon.css">
 
-	<link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="assets/css/magnific-popup.css">
 
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/css/nice-select.css">
 
-	<link rel="stylesheet" href="css/themify-icons.css">
+  <link rel="stylesheet" href="assets/css/slick.css">
 
-	<link rel="stylesheet" href="css/flaticon.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 
-	<link rel="stylesheet" href="css/magnific-popup.css">
-
-	<link rel="stylesheet" href="css/nice-select.css">
-
-	<link rel="stylesheet" href="css/slick.css">
-
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<style >
+  <style >
     td{
       color: black;
     }
-	tr{
-      color: black;
-    }
   </style>
+
+</head>
+
 <body>
-	<br/><br/>
-	<?php include("header.php"); ?>
-	<section class="about_us padding_top">
-	
-			
-	
+<?php include("../public/header.php"); ?>
+<section class="about_us padding_top">
 <div class="container">
-<div class="row justify-content-between align-items-center">
-<div class="col-md-6 col-lg-5">
-<div class="row align-items-center regervation_content">
-<div class="col-lg-7">
-<div class="regervation_part_iner">
-<form><br/><br/>
-<h1>Results</h1>
-<br/><br/>
-<div class="form-row">
-							
-<form class="form-group col-md-6">
-</div>
-<table>
-<tr>
-                    <thead>
-                       <td>ID</td>
-                       <td>Email</td> 
-                       <td>Gender</td> 
-                        <td>Status</td> 
-
-                    </thead>
-<!-- <h4>ID:</h4> <br><br>
-
-
-<h4>Email:</h4> <br><br>
-
-
-<h4>:</h4> <br><br>
-
-
-<h4>:</h4>
-
-<br><br> -->
-<?php
-        $sql = "SELECT * FROM `results`";
-        $result = mysqli_query($connection, $sql);
-        $count = mysqli_num_rows($result);
-        if ($count > 0) {
-            while ($rows = mysqli_fetch_array($result)) {
-                ?>
-                    <td><?php echo $rows[0]; ?></td>
-                    <td><?php echo $rows[1]; ?></td>
-                    <td><?php echo $rows[2]; ?></td>
-					<td><?php echo $rows[3]; ?></td>
-                    
-                </tr>
-                <?php
-            }
-            ?>
-
-            <?php
-
-        } else {
-
-            echo "No data to view";
-
-        }
-
-        ?>
-		</tbody>
-    </table>
-	<br><br>
-	</div>
-<div>
-              <div class="regerv_btn">
-
-        <button type="submit" name="print" class="btn_2" onclick="window.print()"> Print Result</button>
-
+  <div class="row justify-content-between">
+    <div class="col-lg">
+      <div class="regervation_part_iner" style="display: inline-block;">
+        <div class="form-row">
+          
+            <br><br>
+        <h1 >Results</h1>
+        <br>
+        <div class="form-group col-lg-12">
+            <div class="row" id="old">
+                <table>
+                  <tr style="background-color: #4169E1;">
+                    <th>ID</th>
+                    <th>Email</th> 
+                    <th>User Type</th> 
+                    <th>Statuts</th> 
+                   
+                  </tr>
+                  
+           
+                    <tr>
+                            <td>1</td>
+                            <td style=" color: white;">Nura</td>
+                            <td style=" color: white;">Female</td>
+                            <td style=" color: #00FF01;">Psostive</td>
+                        </tr>';                    
+                }
+            } 
+            </table>
+                    <br> <br>
+                
+                        </div>
+            
+            </div>
         </div>
-		</div>
-		<div>
-        <br><br>
- 
-        <div class="regerv_btn">
-        <a href="test.php" class="btn_2">Add New Test</a>
-        </div>
-		</div>
-							
-
-        </form>
-
-		<br/><br/>
-</div>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
+      </div>
+    </div>
 </div>
 </section>
-<?php include("footer.php"); ?>
+<script src="assets/js/jquery-3.4.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/owl.carousel.js"></script>
+<script src="assets/js/main.js"></script>
+<script>
+    new WOW().init();
+</script>
 </body>
 </html>
-
-						
