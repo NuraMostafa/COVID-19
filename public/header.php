087d54c9d6677dc7c9e711698e74444bb872c118
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-07-11 20:44:29 +0200

    Update header.php

--- public/header.php
+++ public/header.php
@@ -48,7 +48,7 @@ require_once("../app/db/dbh.php");
 <div class="row align-items-center">
 <div class="col-lg-12">
 <nav class="navbar navbar-expand-lg navbar-light">
-<a href="Home.php"> <img src="assets/img/Coronavirus-Raw-Materials.png" alt="logo"> </a>
+<a href="index.php"> <img src="assets/img/Coronavirus-Raw-Materials.png" alt="logo"> </a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
