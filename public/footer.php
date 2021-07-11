8eda75aef00ec18e878dffb38a6730dfc59ce9f1
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-07-11 20:46:45 +0200

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("connect.php");
+require_once("../app/db/dbh.php");
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,25 +9,25 @@ require_once("connect.php");
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>COVID</title>
-<link rel="icon" href="img/favicon.png">
+<link rel="icon" href="assets/img/favicon.png">
 
-<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
-<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="assets/css/animate.css">
 
-<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
 
-<link rel="stylesheet" href="css/themify-icons.css">
+<link rel="stylesheet" href="assets/css/themify-icons.css">
 
-<link rel="stylesheet" href="css/flaticon.css">
+<link rel="stylesheet" href="assets/css/flaticon.css">
 
-<link rel="stylesheet" href="css/magnific-popup.css">
+<link rel="stylesheet" href="assets/css/magnific-popup.css">
 
-<link rel="stylesheet" href="css/nice-select.css">
+<link rel="stylesheet" href="assets/css/nice-select.css">
 
-<link rel="stylesheet" href="css/slick.css">
+<link rel="stylesheet" href="assets/css/slick.css">
 
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="assets/css/style.css">
 <style>
 	a {
 		color: #2554C7;
@@ -44,8 +44,8 @@ require_once("connect.php");
 <div class="container">
 <div class="row justify-content-between">
 <div class="col-xl-2 col-md-4 col-sm-6 single-footer-widget">
-<a href="#" class="footer_logo"> <img src="img/nura.jpeg" alt="#"> </a>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+<a href="#" class="footer_logo"> <img src="assets/img/Coronavirus-Raw-Materials.png" alt="#"> </a>
+<p>COVID-19 is an infectious disease that mainly affects lungs. Pneumonia is also an infection that causes inflammation in the air sacs in lungs. </p>
 <div class="social_logo">
 <a href="#"><i class="ti-facebook"></i></a>
   <a href="#"> <i class="ti-twitter"></i> </a>
@@ -56,48 +56,16 @@ require_once("connect.php");
 <div class="col-xl-2 col-sm-6 col-md-4 single-footer-widget">
 <h4>Quick Links</h4>
 <ul>
-<li><a href="#">About us</a></li>
-<li><a href="#">Department</a></li>
-<li><a href="#"> Online payment</a></li>
-<li><a href="#">Careers</a></li>
-<li><a href="#">Department</a></li>
+<li><a href="index.php">About us</a></li>
+<li><a href="user.php?type=register"> SignUp</a></li>
+<li><a href="user.php">Login</a></li>
 </ul>
 </div>
-<div class="col-xl-2 col-sm-6 col-md-4 single-footer-widget">
-<h4>Explore</h4>
-<ul>
-<li><a href="#">In the community</a></li>
-<li><a href="#">IU health foundation</a></li>
-<li><a href="#">Family support </a></li>
-<li><a href="#">Business solution</a></li>
-<li><a href="#">Community clinic</a></li>
-</ul>
- </div>
-<div class="col-xl-2 col-sm-6 col-md-6 single-footer-widget">
-<h4>Resources</h4>
-<ul>
-<li><a href="#">Lights were season</a></li>
-<li><a href="#"> Their is let wherein</a></li>
-<li><a href="#">which given over</a></li>
-<li><a href="#">Without given She</a></li>
-<li><a href="#">Isn two signs think</a></li>
-</ul>
-</div>
+
 <div class="col-xl-3 col-sm-6 col-md-6 single-footer-widget">
-<h4>Newsletter</h4>
-<p>Seed good winged wherein which night multiply
-midst does not fruitful</p>
-<div class="form-wrap" id="mc_embed_signup">
-<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
-<input class="form-control" name="EMAIL" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address '" required="" type="email">
-<button class="click-btn btn btn-default text-uppercase"> <i class="ti-angle-right"></i>
-</button>
-<div style="position: absolute; left: -5000px;">
-<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-</div>
-<div class="info"></div>
-</form>
-</div>
+<h4>Quote</h4>
+<p>We Care About Your Health</p>
+
 </div>
 </div>
 </div>
@@ -105,9 +73,8 @@ midst does not fruitful</p>
 <div class="copyright_part">
 <div class="container">
 <div class="row align-items-center">
-<p class="footer-text m-0 col-lg-8 col-md-12">
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-
+<p class="footer-text m-0 col-lg-12">
+	Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved
 </p>
 <div class="col-lg-4 col-md-12 text-center text-lg-right footer-social">
 <a href="#"><i class="ti-facebook"></i></a>
