51642250a38147972bcc24d6855252b8384da3a8
NoranHanyy <65794035+NoranHanyy@users.noreply.github.com>
2021-07-13 02:32:11 +0200

    Update Flask.py

--- python models/Flask.py
+++ python models/Flask.py
@@ -24,19 +24,6 @@ def uploadText():
         writer.writerow(data)
     predicttests=classifytests('bloodtests.csv')
     return predicttests
-# import csv
-#
-# header = ['name', 'area', 'country_code2', 'country_code3']
-# data = ['Afghanistan', 652090, 'AF', 'AFG']
-#
-# with open('countries.csv', 'w', encoding='UTF8') as f:
-#     writer = csv.writer(f)
-#
-#     # write the header
-#     writer.writerow(header)
-#
-#     # write the data
-#     writer.writerow(data)
 
 
 @app.route('/uploadImage', methods=['POST'])
